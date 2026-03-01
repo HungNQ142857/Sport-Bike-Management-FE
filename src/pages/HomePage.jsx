8ff0127eefc09f3753f2a5316298d77a38e38456
phucduchaha <ducdpse171074@fpt.edu.vn>
2026-03-02 02:20:42 +0700

    Setup Axios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import axiosClient from "../services/axiosClient";
+
 export default function HomePage() {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Home Page 🚴
-        </h1>
-      </div>
-    );
-  }
+  useEffect(() => {
+    axiosClient
+      .get("/test")
+      .then((response) => {
+        console.log("API response:", response.data);
+      })
+      .catch((error) => {
+        console.log("API error:", error.message);
+      });
+  }, []);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Home Page 🚴
+      </h1>
+    </div>
+  );
+}
